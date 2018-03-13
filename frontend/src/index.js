e08cf3e6e58acefe111c5c8bbd4d19d2bf9207a6
Bryan Perino <Bryan.Perino@gmail.com>
2018-03-12 21:54:17 -0400

    Fixed some more styling for the smaller display. Fixed refresh callback

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,13 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
+import { connect } from "react-redux";
 
 import "./index.css";
 import Main from "./routes/Main/Main";
 import RouteDetail from "./routes/RouteDetail/RouteDetail";
 import InformationStatusBarContainer from "./InformationStatusBarContainer/InformationStatusBarContainer";
-import { STATUSES_RECEIVED } from "./actions";
+import { STATUSES_RECEIVED, retrieveStatuses } from "./actions";
 
 const defaultState = {
   isLoading: true,
@@ -35,17 +36,15 @@ const reducer = (state = defaultState, action) => {
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
-const refreshStatusList = () => this.props.retrieveStatuses();
-
-const Wrapper = props => (
+const Wrapper = connect(state => ({}), { retrieveStatuses })(props => (
   <div>
     <InformationStatusBarContainer
       refreshIntervalSeconds={120}
-      refreshCallback={refreshStatusList}
+      refreshCallback={props.retrieveStatuses}
     />
     {props.children}
   </div>
-);
+));
 
 const run = () =>
   ReactDOM.render(
@@ -66,6 +65,7 @@ const run = () =>
     </Provider>,
     document.getElementById("root")
   );
+
 store.subscribe(run);
 run();
 registerServiceWorker();
